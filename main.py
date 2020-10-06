73c6c23cb000fe44777933c8140621bd8cb3af41
wangyu-ustc <1294837640@qq.com>
2020-10-06 12:16:06 +0800

    update main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from best_syn import *
 if __name__ == '__main__':
     tr4w = TextRank4Keyword()
 
-    Question = input("Input a Question:")
+    # Question = input("Input a Question:")
+    Question = 'Where is USTC?'
+    key_list = []
+    # get the interrogative of this sentence
+    for word in Question.lower().split(' '):
+        if word in ['who', 'where', 'how', 'why']:
+            key_list.append(word)
+
+    # get the other key words
     tr4w.analyze(text=Question, lower=True, window=2)
     for item in tr4w.get_keywords(20, word_min_len=1):
-        print(item.word, item.weight)
+        # print(item.word, item.weight)
+        key_list.append(item.word)
 
+    print(key_list)
 
 
